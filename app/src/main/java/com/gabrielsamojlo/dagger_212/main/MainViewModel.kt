ef05edd4ec1206c86f4a308f51d5c5a55e2cff90
AzureAD\RafiSadat <rsadat@datagene.com.au>
2019-04-11 13:33:36 +1000

    no message

---
+++ app/src/main/java/com/gabrielsamojlo/dagger_212/main/MainViewModel.kt
@@ -0,0 +1,5 @@
+package com.gabrielsamojlo.dagger_212.main
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel(var test: String) : ViewModel()
---
+++ app/src/main/java/com/gabrielsamojlo/dagger_212/main/fragment/MainFragmentViewModel.kt
@@ -0,0 +1,5 @@
+package com.gabrielsamojlo.dagger_212.main.fragment
+
+import android.arch.lifecycle.ViewModel
+
+class MainFragmentViewModel(var anotherTest: String) : ViewModel()
